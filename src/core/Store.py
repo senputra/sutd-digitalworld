057fc42ebac7fd5f3706071379ff5d7b6bef3bfb
Dody <dodysenz@gmail.com>
2020-02-20 01:44:09 +0800

    Simplified Store

--- src/core/Store.py
+++ src/core/Store.py
@@ -3,22 +3,53 @@ from core.helper import Logger
 
 
 class Store:
+
+    class StoreTYPE:
+        WASHING_MACHINE = "washing_machine"
+        DRYING_MACHINE = "drying_machine"
+
     """ Store class to implement redux in python """
 
-    _reducer: Reducer
-    _state: dict
+    # _reducer: Reducer
+    # _state: dict
 
-    def __init__(self, reducer: Reducer, verbose=False) -> None:
+    _cache: dict = {
+        "washing_machine": {},
+        "drying_machine": {},
+    }
+
+    def __init__(self, verbose=False) -> None:
         super().__init__()
 
-        self._reducer = reducer
         self._verbsoe = verbose
+        self._callbacks = []
 
-    def dispatch(self, action: Action) -> None:
-        self._reducer.convertActionToState(self._state, action)
-        _log(action.getName())
+    def upsert(self, feature: str, id: str, value) -> None:
+        if (feature not in _cache.keys()):
+            _log("ERROR: feature not found")
+            return
+        self._cache[feature][id] = value
+
+        # Notifies listeners on changes in values
+        self.notify()
         return
+
+    def addListener(self, callbackFn):
+        return self._callbacks.append(callbackFn)
+
+    def removeListener(self, callbackFn):
+        return self._callbacks.remove(callbackFn)
+
+    def notify(self):
+        for callback in self._callbacks:
+            callback()
+        return
+
+    # def dispatch(self, action: Action) -> None:
+    #     self._reducer.convertActionToState(self._state, action)
+    #     _log(action.getName())
+    #     return
 
     def _log(self, message) -> None:
         if self._verbose is True:
-            Logger.logToConsole(message)
+            Logger.logToConsole("[STORE] " + message)
--- src/facade.py
+++ src/facade.py
@@ -1,6 +1,8 @@
 from core.WashingMachine.Constants import STATUS
 from core.Action import Action
 from core.WashingMachine.Actions.Update import Update
+from core.Store import Store
+from core.Reducer import Reducer
 
 
 class Facade:
@@ -10,7 +12,30 @@ class Facade:
         request_machin
     """
 
-    def _dispatch(self, action: Action) -> None:
+    def __init__(self):
+        super().__init__()
+        self.store = Store(verbose=True)
+
+    def requestDataFromFirestore(self) -> None:
+        # Call a funtion from Long&Weisi's code and bind with a callback
+        return
+
+    def listeningToWashingMachineStatus(self, callback) -> None:
+        """ listening to the status of wanted object"""
+        '''
+            bind a callback 
+            if there is a new data
+            call the callback with the payload
+
+
+            Data is in dict: {
+                "BLK55_WM_1" : "occupied",
+                .
+                .
+                .
+            }
+        '''
+        callback(self._washingMachineData)
         return
 
     def updateWashingMachine(self, machineId: str, status: str) -> None:
