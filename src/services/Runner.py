f2c4ae3d6f83b3ab50d585235b32aa047d493049
Coconut <d>
2020-03-10 01:04:33 +0800

    For Long

---
+++ src/services/Runner.py
@@ -0,0 +1,13 @@
+class Runner:
+
+    """
+    set interval 
+
+    1. Takes in the function and the arguments that it has to run 
+    2. After the timeInterval
+
+    https://stackoverflow.com/questions/12435211/python-threading-timer-repeat-function-every-n-seconds/16368571#16368571
+    https://stackoverflow.com/questions/22498038/improve-current-implementation-of-a-setinterval-python/22498708#22498708
+    """
+    def set_interval(self, function, timeInterval:int):
+        pass 
