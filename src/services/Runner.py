127a9398e51a3eee2b4481123ba79064ec3b2305
Long <long_luu@mymail.sutd.edu.sg>
2020-03-13 14:31:02 +0800

    Interval functions

--- src/services/Runner.py
+++ src/services/Runner.py
@@ -1,20 +1,53 @@
+from threading import Event, Thread
 from services import Logger
+
+from AdminService import *
+
 class Runner:
+    
+    __new_thread = None
+    __stopped = Event()
+    def __init__(self, machineID):
+        self.__machineID = machineID
+      
+        #print("Thread initiated")
+        
 
-    _logger = None
+      
+    #set interval 
 
-    def __init__(self):
-        self._logger = Logger.Logger()
+    #1. Takes in the function and the arguments that it has to run 
+    #2. After the timeInterval
+    def call_repeatedly(self, interval, func, *args):
+        def loop():
+            while not self.__stopped.wait(interval): # the first call is in `interval` secs
+                
+                func(*args)
+                break
+            return self.__stopped.clear()
+            
+        #print("An iteration")
+        self.__new_thread = Thread(target=loop)    
+        self.__new_thread.start()
+        return self.__stopped.set
+    
+    def end_timer(self):
+        return not self.__new_thread.is_alive()
+   
 
-    """
-    set interval 
+'''
+# Sample debugging code
+b = Booking()
+mac = ["Block57_Dryer_01", "Block57_Dryer_02", "Block57_Dryer_03", "Block55_Dryer_01", "Block55_Washer_01"]
 
-    1. Takes in the function and the arguments that it has to run 
-    2. After the timeInterval
+for i in mac:    
+    a = Runner(i)
+    Loc, Type, ID = b.ID_splicer(i)
+    
+    a.call_repeatedly(10, b.undo_occupy_machine, Loc, Type, ID) 
+    if a.end_timer():
+        del a #should delete the object
+'''
 
-    https://stackoverflow.com/questions/12435211/python-threading-timer-repeat-function-every-n-seconds/16368571#16368571
-    https://stackoverflow.com/questions/22498038/improve-current-implementation-of-a-setinterval-python/22498708#22498708
-    """
-    def set_interval(self, function, timeInterval:int):
-        self._logger.log("NOT IMPLEMENTED")
-        pass 
+
+    
